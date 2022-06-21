d1c791d6183d0ee108a2a9429908175c24852715
E-M-Cay <82440817+E-M-Cay@users.noreply.github.com>
2022-06-21 14:09:38 +0200

    db migration ok

--- db/AppDataSource.ts
+++ db/AppDataSource.ts
@@ -4,10 +4,10 @@ const AppDataSource = new DataSource({
   type: 'mariadb',
   host: 'j5zntocs2dn6c3fj.chr7pe7iynqr.eu-west-1.rds.amazonaws.com',
   port: 3306,
-  username: 'oxoql4kfui8g6j06',
-  password: 'p52uj1cxaa204dll',
-  database: 't5ipem5ur4w70u2m',
-  entities: ['entities/*{.js,.ts}'],
+  username: 'jqw2ja5erkzupc85',
+  password: 'p1wrm3zmcpgt49ts',
+  database: 'swrnmpcftzalh7to',
+  entities: ['entities/*.ts'],
   migrations: ['migrations/*{.js,.ts}'],
 });
 

d87c5907a4f228b90c706fd3a6015baf22f15164
ArthurVanier <arthur.vanier@epitech.eu>
2022-06-28 13:13:01 +0200

    add CRUD server

--- routes/server/index.ts
+++ routes/server/index.ts
@@ -33,4 +33,49 @@ router.get('/list', isAuth,  async (req:IRequest, res:Response) => {
     return res.send(server_object_list)
 })
 
+router.create('/create_server', isAuth, async (req:IRequest, res:Response) => {
+    if('name' in req.body && 'main_img' in req.body){
+        const name:string = req.body.name
+        const main_img:string = req.body.main_img
+        try{
+            const server:Server = ServerRepository.create({
+                'name': name,
+                'main_img': main_img
+            })
+    
+            ServerRepository.save(server)
+            return res.status(200).send(server)
+        }catch(error){
+            return res.status(400).send('Error')
+        }
+    }
+    return res.status(400).send('Wrong arguments')
+    
+})
+
+router.create('/update_server', isAuth, async (req:IRequest, res:Response) => {
+    if(('main_img' in req.body || 'logo' in req.body) && 'server_id' in req.body){
+        const server = await ServerRepository.findOneBy({id: Number(req.body.server_id)})
+        const logo:string = ('logo' in req.body) ? req.body.name : null
+        const main_img:string = ('main_img' in req.body) ? req.body.main_img : null
+
+        if(!server)
+            return res.status(400).send('Error server not found')
+
+        try{
+            if(logo)
+                server.logo = logo
+            if(main_img)
+                server.main_img = main_img
+            
+            ServerRepository.save(server)
+            return res.status(200).send(server)
+        }catch(error){
+            return res.status(400).send('Error')
+        }
+    }
+    return res.status(400).send('Wrong arguments')
+})
+
+
 module.exports = router;

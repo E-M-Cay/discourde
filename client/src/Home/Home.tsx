41964d4e57e2df5ed5beebfcc6227c6e35276593
Nathan Girard <ngirard@amxonesolutions.com>
2022-07-05 11:24:46 +0200

    ui

--- client/src/Home/Home.tsx
+++ client/src/Home/Home.tsx
@@ -2,6 +2,7 @@ import react from "react";
 import {LeftBar} from "../LeftBar/LeftBar";
 import {Main} from "../Main/Main";
 import {Col, Row} from "antd";
+import { ChanelBar } from "../ChanelBar/ChanelBar";
 
 export const Home = () => {
     return (
--- client/src/Main/Main.tsx
+++ client/src/Main/Main.tsx
@@ -1,14 +1,16 @@
-import {Content} from "antd/es/layout/layout";
-import {Col, Input, Layout, Row} from "antd";
+import { Content } from "antd/es/layout/layout";
+import { Col, Input, Layout, Row } from "antd";
 import Sider from "antd/es/layout/Sider";
+import { ChanelBar } from "../ChanelBar/ChanelBar";
+import chanelData from "../mock1";
 
 export const Main = () => {
     return (
-        <Row style={{height: "100vh",width: "100%", marginLeft: "0 !important"}} className="main">
-        
-        <Col style={{backgroundColor: "grey"}} span={4}>hjqdshjld</Col>
-        <Col span={16}>qdsdsqdqd</Col>
-        <Col style={{backgroundColor: "grey"}} span={4}>ksqkhjds</Col>
+        <Row style={{ height: "100vh", width: "100%", marginLeft: "0 !important" }} className="main">
+
+            <Col style={{ backgroundColor: "#535151" }} span={3.5}><ChanelBar></ChanelBar></Col>
+            <Col span={16}>qdsdsqdqd</Col>
+            <Col style={{ backgroundColor: "grey" }} span={4}>ksqkhjds</Col>
         </Row>
     )
 }

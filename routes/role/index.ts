25866212f966bbcca34294903c9067f84f3909db
ArthurVanier <arthur.vanier@epitech.eu>
2022-08-14 17:41:18 +0200

    can create rolepermission et serveruserrole when creating role fro a server

--- routes/role/index.ts
+++ routes/role/index.ts
@@ -12,11 +12,17 @@ import { In } from 'typeorm';
 import { ServerUser } from '../../entities/ServerUser';
 
 import isAuth from '../../MiddleWares/isAuth';
+import { Permission } from '../../entities/Permission';
+import { ServerUserRole } from '../../entities/ServerUserRole';
+import { RolePermission } from '../../entities/RolePermission';
 
 const UserRepository = AppDataSource.getRepository(User);
 const ServerRepository = AppDataSource.getRepository(Server);
 const ServerUserRepository = AppDataSource.getRepository(ServerUser);
 const RoleRepository = AppDataSource.getRepository(Role);
+const PermissionRepository = AppDataSource.getRepository(Permission)
+const serverUserRoleRepository = AppDataSource.getRepository(ServerUserRole)
+const RolePermissionRepository = AppDataSource.getRepository(RolePermission)
 
 router.get('/list/:server_id', isAuth, async (req: IRequest, res: Response) => {
     const server_id = Number(req.params.server_id);
@@ -40,14 +46,42 @@ router.post('/create/', isAuth, async (req: IRequest, res: Response) => {
 
         if (!server) return res.status(400).send('Error server not found');
 
-        try {
+        try{
             const role = RoleRepository.create({
-                name: name,
-            });
-            await RoleRepository.save(role);
-            return res.status(200).send(role);
-        } catch (error) {
-            return res.status(400).send('Error');
+                name: name
+            })
+            await RoleRepository.save(role)
+
+            if('permissions' in req.body){
+                for(const permission_id of req.body.permissions){
+                    const permission = await PermissionRepository.findOneBy({id: permission_id})
+                    if(!permission)
+                        continue
+                    
+                    const role_permisison = RolePermissionRepository.create({
+                        role: role,
+                        permission: permission
+                    })
+                    await RolePermissionRepository.save(role_permisison)
+                }
+            }
+            
+            if('users' in req.body){
+                for(const user_id of req.body.users){
+                    const server_user = await ServerUserRepository.findOneBy({user: user_id, server: server})
+                    if(!server_user)
+                        continue
+                    
+                    const server_role_user = serverUserRoleRepository.create({
+                        user: server_user,
+                        role: role
+                    })
+                    await serverUserRoleRepository.save(server_role_user)
+                }
+            }
+            return res.status(200).send(role)
+        }catch(error){
+            return res.status(400).send('Error')
         }
     }
     return res.status(400).send('Wrong arguments');

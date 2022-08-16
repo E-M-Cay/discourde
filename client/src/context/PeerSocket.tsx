7f9fda04df3104a070b51c0dd6e92e385e17ab4e
E-M-Cay <82440817+E-M-Cay@users.noreply.github.com>
2022-08-16 21:14:53 +0200

    add turn sever ?

--- client/src/context/PeerSocket.tsx
+++ client/src/context/PeerSocket.tsx
@@ -5,87 +5,94 @@ import { peerPort, peerServerHost } from '../env/host';
 import Peer from 'peerjs';
 
 interface PeerSocket {
-    peer?: Peer;
-    socket?: Socket;
-    connectPeer: () => void;
-    connectSocket: (token: string) => void;
+  peer?: Peer;
+  socket?: Socket;
+  connectPeer: () => void;
+  connectSocket: (token: string) => void;
 }
 
 const PeerSocketContext = createContext<PeerSocket>({
-    connectPeer: () => {
-        throw new Error('connectPeer not correctly overriden');
-    },
-    connectSocket: () => {
-        throw new Error('connectSocket not correctly overriden');
-    },
+  connectPeer: () => {
+    throw new Error('connectPeer not correctly overriden');
+  },
+  connectSocket: () => {
+    throw new Error('connectSocket not correctly overriden');
+  },
 });
 
 interface Props {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
 
 const PeerSocketProvider: React.FunctionComponent<Props> = ({ children }) => {
-    interface ISocket extends Socket {
-        username?: string;
+  interface ISocket extends Socket {
+    username?: string;
+  }
+
+  const [socket, setSocket] = useState<ISocket>();
+  const [peer, setPeer] = useState<Peer>();
+
+  const connectSocket = (token: string) => {
+    if (process.env.NODE_ENV === 'production') {
+      setSocket(
+        io(`wss://${host}:${socketPort}/`, {
+          auth: {
+            token,
+          },
+        })
+      );
+    } else {
+      setSocket(
+        io(`ws://${host}:${socketPort}/`, {
+          auth: {
+            token,
+          },
+        })
+      );
+    }
+  };
+
+  const connectPeer = () => {
+    console.log('connect peer');
+    let secure = false;
+    if (process.env.NODE_ENV === 'production') {
+      secure = true;
     }
 
-    const [socket, setSocket] = useState<ISocket>();
-    const [peer, setPeer] = useState<Peer>();
+    const newPeer = new Peer({
+      config: {
+        iceServers: [
+          {
+            urls: [
+              'stun:stun.l.google.com:19302',
+              'stun1.l.google.com:19302',
+              'stun2.l.google.com:19302',
+            ],
+          },
+          {
+            urls: 'turn:turn.bistri.com:80',
+            credential: 'homeo',
+            username: 'homeo',
+          },
+        ],
+        sdpSemantics: 'unified-plan',
+      },
+      port: peerPort,
+      host: peerServerHost,
+      path: '/',
+      debug: 3,
+      secure,
+    });
+    setPeer(newPeer);
+  };
 
-    const connectSocket = (token: string) => {
-        if (process.env.NODE_ENV === 'production') {
-            setSocket(
-                io(`wss://${host}:${socketPort}/`, {
-                    auth: {
-                        token,
-                    },
-                })
-            );
-        } else {
-            setSocket(
-                io(`ws://${host}:${socketPort}/`, {
-                    auth: {
-                        token,
-                    },
-                })
-            );
-        }
-    };
-
-    const connectPeer = () => {
-        console.log('connect peer');
-        let secure = false;
-        if (process.env.NODE_ENV === 'production') {
-            secure = true;
-        }
-
-        const newPeer = new Peer({
-            config: {
-                iceServers: [
-                    { urls: ['stun:stun.l.google.com:19302'] },
-                    {
-                        urls: 'turn:turn.bistri.com:80',
-                        credential: 'homeo',
-                        username: 'homeo',
-                    },
-                ],
-                sdpSemantics: 'unified-plan',
-            },
-            port: peerPort,
-            host: peerServerHost,
-            path: '/',
-            debug: 3,
-            secure,
-        });
-        setPeer(newPeer);
-    };
-
-    return (
-        <PeerSocketContext.Provider
-            value={{ peer, socket, connectPeer, connectSocket }}>
-            {children}
-        </PeerSocketContext.Provider>
-    );
+  return (
+    <PeerSocketContext.Provider
+      value={{ peer, socket, connectPeer, connectSocket }}
+    >
+      {children}
+    </PeerSocketContext.Provider>
+  );
 };
 
 export default PeerSocketProvider;

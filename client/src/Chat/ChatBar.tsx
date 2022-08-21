e43c888d81365c0d6e826ccd25584970d82675bc
renaudababsa <renaud.ababsa@epitech.eu>
2022-08-21 02:48:28 +0200

    smiley

--- client/src/Chat/ChatBar.tsx
+++ client/src/Chat/ChatBar.tsx
@@ -1,14 +1,23 @@
-import React, { useContext, useState } from 'react';
+import React, {
+  useCallback,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
 import 'antd/dist/antd.min.css';
 import { Input, Form } from 'antd';
 import { PeerSocketContext } from '../context/PeerSocket';
 import { useAppSelector } from '../redux/hooks';
 import Picker from 'emoji-picker-react';
 import { CloseOutlined, SmileOutlined } from '@ant-design/icons';
+import type { InputRef } from 'antd';
 
 const ChatBar = () => {
   const { socket } = useContext(PeerSocketContext);
   const [input, setInput] = useState<string>('');
+  const node = useRef<HTMLDivElement>(null);
+  const inputRef = useRef<InputRef>(null);
   const activeChannel = useAppSelector(
     (state) => state.userReducer.activeChannel
   );
@@ -22,11 +31,34 @@ const ChatBar = () => {
     setInput('');
   };
 
-  const [isSmiley, setIsSmiley] = useState(false);
+  const [isSmiley, setIsSmiley] = useState(true);
 
   const onEmojiClick = (event: React.MouseEvent, emojiObject: any) => {
     setInput(input + emojiObject.emoji);
+    inputRef.current?.focus();
+    setIsSmiley(false);
   };
+  const handleOutsideClick = useCallback(
+    (e: any) => {
+      if (node.current && node.current.contains(e.target)) {
+        return;
+      }
+      console.log('outside click', node);
+      setIsSmiley(false);
+    },
+    [node]
+  );
+  useEffect(() => {
+    if (isSmiley) {
+      document.addEventListener('click', handleOutsideClick, false);
+    } else {
+      document.removeEventListener('click', handleOutsideClick, false);
+    }
+
+    return () => {
+      document.removeEventListener('click', handleOutsideClick, false);
+    };
+  }, [isSmiley, handleOutsideClick]);
 
   // const user = useAppSelector((state) => state.userReducer);
 
@@ -44,36 +76,49 @@ const ChatBar = () => {
   return (
     <div className='chatbar'>
       <Form style={{ width: '100%' }} onSubmitCapture={onSubmitHandler}>
-        <Input
-          bordered={false}
-          className='inputMain'
-          placeholder='Envoyer un message dans '
-          onChange={(e) => setInput(e.target.value)}
-          value={input}
-          addonAfter={
-            <SmileOutlined
-              onClick={() => setIsSmiley(!isSmiley)}
-              style={{ fontSize: '15px', color: '#A1A1A1' }}
-            />
-          }
-        />
+        <div ref={node}>
+          <Input
+            bordered={false}
+            className='inputMain'
+            placeholder='Envoyer un message dans '
+            id='inputReturn'
+            onChange={(e) => setInput(e.target.value)}
+            value={input}
+            ref={inputRef}
+          />
+          <SmileOutlined
+            onClick={() => setIsSmiley(!isSmiley)}
+            className='smileyA'
+            style={{
+              position: 'relative',
+              fontSize: '20px',
+              fontWeight: 'bold',
+              color: '#A1A1A1',
+              left: '1290px',
+              bottom: '33px',
+              padding: '8px',
+            }}
+          />
+        </div>
         {isSmiley && (
           <div
-            style={{ position: 'relative', bottom: '355px', right: '-1045px' }}
+            style={{ position: 'relative', bottom: '394px', right: '-1045px' }}
           >
             <Picker
               disableSearchBar
-              disableAutoFocus={true}
+              disableAutoFocus
               onEmojiClick={onEmojiClick}
-            />
-            <CloseOutlined
-              style={{
-                position: 'relative',
-                bottom: '322px',
-                left: '2px',
-                cursor: 'pointer',
+              groupNames={{
+                smileys_people: 'visages',
+                animals_nature: 'animaux et nature',
+                food_drink: 'boisson et nourriture',
+                travel_places: 'voyage et lieu',
+                activities: 'jeux et activité',
+                objects: 'objets',
+                symbols: 'symboles',
+                flags: 'drapeaux',
+                recently_used: 'utilisé récemment',
               }}
-              onClick={() => setIsSmiley(!isSmiley)}
             />
           </div>
         )}

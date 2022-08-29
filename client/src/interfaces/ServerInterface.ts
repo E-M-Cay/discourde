4caaf570eaf1aa3c1160c255a11e329ff6bdabce
renaudababsa <renaud.ababsa@epitech.eu>
2022-08-29 12:45:30 +0200

    devupdate

--- client/src/interfaces/ServerInterface.ts
+++
@@ -1,8 +0,0 @@
-interface Server {
-  id: number;
-  logo: string;
-  main_img: string;
-  name: string;
-}
-
-export default Server;
--- client/src/utils.ts
+++
@@ -1,3 +0,0 @@
-import { useAppSelector } from './redux/hooks';
-
-const user = useAppSelector((state) => state.userReducer);
